87e7fd0ab90af7ee7056af81698210937f51ed19
maplewing <maplewing@rayark.com>
2019-03-23 12:03:51 +0800

    Implement `Camera follow character`.

--- Assets/Scripts/GamePlayScene.cs
+++ Assets/Scripts/GamePlayScene.cs
@@ -6,11 +6,17 @@ namespace Rayark.Hi
 
     public class GamePlayScene : MonoBehaviour
     {
+        private const float CAMERA_CHARACTER_DIFF_Z = -10;
+
+
         [SerializeField]
         private CharacterData _characterData;
 
         [SerializeField]
         private Transform _characterTransform;
+
+        [SerializeField]
+        private Transform _cameraTransform;
 
         private HiEngine _hiEngine;
 
@@ -22,10 +28,15 @@ namespace Rayark.Hi
         void Update()
         {
             _hiEngine.Update(Time.deltaTime);
+
             _characterTransform.localPosition = new Vector3(
                 _characterTransform.localPosition.x,
                 _characterTransform.localPosition.y,
                 _hiEngine.CurrentCharacterPosition.y);
+            _cameraTransform.localPosition = new Vector3(
+                _cameraTransform.localPosition.x,
+                _cameraTransform.localPosition.y,
+                _hiEngine.CurrentCharacterPosition.y + CAMERA_CHARACTER_DIFF_Z);
         }
     }
 }
